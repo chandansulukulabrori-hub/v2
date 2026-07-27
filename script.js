4049f10bf5eb4f105f8b9041fa0edac78b30faa3
chandansulukulabrori-hub <chandansulukulabrori@gmail.com>
2026-07-27 12:18:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 const launchButton = document.getElementById("launchButton");
+const countdown = document.getElementById("countdown");
 
-launchButton.addEventListener("click",()=>{
+launchButton.addEventListener("click",startLaunch);
 
-    alert("Artemis II Ready 🚀");
+async function startLaunch(){
 
-});
+    launchButton.style.display="none";
+
+    const numbers=["3","2","1","LIFTOFF 🚀"];
+
+    for(let i=0;i<numbers.length;i++){
+
+        countdown.innerHTML=numbers[i];
+
+        countdown.classList.add("show");
+
+        await delay(1000);
+
+        countdown.classList.remove("show");
+
+        await delay(300);
+
+    }
+
+}
+
+function delay(ms){
+
+    return new Promise(resolve=>setTimeout(resolve,ms));
+
+}
