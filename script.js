b66b009e2d9b3c628b9492295445a66a95d9772b
chandansulukulabrori-hub <chandansulukulabrori@gmail.com>
2026-07-27 12:41:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 const launchButton = document.getElementById("launchButton");
 const countdown = document.getElementById("countdown");
 
-console.log(launchButton);
-console.log(countdown);
+launchButton.addEventListener("click",startLaunch);
+
+async function startLaunch(){
+
+    launchButton.style.display="none";
+
+    const numbers=["3","2","1","LIFTOFF 🚀"];
+
+    for(let i=0;i<numbers.length;i++){
+
+        countdown.innerHTML=numbers[i];
+
+        countdown.classList.add("show");
+
+        await delay(1000);
+
+        countdown.classList.remove("show");
+
+        await delay(300);
+
+    }
+
+}
+
+function delay(ms){
+
+    return new Promise(resolve=>setTimeout(resolve,ms));
+
+}
